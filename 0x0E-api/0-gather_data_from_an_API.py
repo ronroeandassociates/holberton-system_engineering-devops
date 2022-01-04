ef44341757f83eda1de296ef674e818ac352ac6c
Ronroeandassociates <3321@holbertonschool.com>
2022-01-04 08:36:08 -0600

    Initial setup files for 0x0E. API

---
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+'''
+    Python script that, using this REST API, 
+    for a given employee ID, 
+    returns information about his/her 
+    TODO list progress
+'''
---
+++ 0x0E-api/1-export_to_CSV.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+'''
+    extend Python script to export data 
+    in the CSV format
+'''
